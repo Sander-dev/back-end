248722162eb70b7d7efaaa2ee132758894cc9fed
Sander-dev <felipe.sander350@gmail.com>
2024-09-14 16:05:45 -0300

    feat: Implementação do middleware authenticateToken para autenticar e verificar tokens JWT

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 declare global {
@@ -8,4 +9,19 @@ declare global {
   }
 }
 
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(' ')[1];
 
+  if (!token) {
+    return res.status(401).json({ message: 'Token não fornecido' });
+  }
+
+  jwt.verify(token, 'secret_key', (err, decodedUser) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token inválido ou expirado' });
+    }
+
+    req.user = decodedUser;
+    next();
+  });
+};
